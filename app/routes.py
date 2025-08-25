66e0ebbe2b51114d88205144e564c6580df9de3a
Muhammad Ahad <ahad97140@gmail.com>
2025-08-25 20:24:45 +0500

    commited

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,6 @@
 
 # --- Imports ---
 from flask import request, jsonify
-from flask_mail import Mail
 from .controllers.user_controllers import userController
 from .models.db import get_db_connection
 from .models.schemas import users_table
@@ -13,15 +12,12 @@ from .models.schemas import users_table
 # --- Route Function ---
 def init_routes(app):
 
-    # --- Initialize Flask-Mail with the app ---
-    mail = Mail(app)
-
     db_connection = get_db_connection()
     if db_connection:
         users_table(db_connection)
 
     # Pass the mail instance to the userController
-    user_controller = userController(db_connection, mail)
+    user_controller = userController(db_connection)
 
     # --- API Routes ---
 
